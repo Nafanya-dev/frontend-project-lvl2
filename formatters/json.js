abae5ba7620ecffeda73828c6179bb9d9f03fa82
Vyacheslav Maletin <Maletin.V95@gmail.com>
2022-01-14 02:02:08 +0500

    8.5

--- formatters/json.js
+++ formatters/json.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 const json = (treeDeep) => {
+  const previous = [null];
   const getObj = (tree) => tree
     .reduce((acc, file) => {
       const key = !file.name ? Object.keys(file)[0] : file.name;
@@ -10,8 +11,16 @@ const json = (treeDeep) => {
       } else {
         value[0] = file.children === 'null' ? null : _.cloneDeep(file.children);
       }
-      return [...acc, { name: key, type: file.type, value: value[0] }];
-    }, []);
+      if (file.type === 'deleted' && file.beenUpdated) {
+        previous[0] = _.cloneDeep(file);
+        return acc;
+      } if (file.type === 'added' && file.beenUpdated) {
+        const oldValue = _.cloneDeep(previous[0].children);
+        previous[0] = null;
+        return { ...acc, [key]: ['updated', oldValue, value[0]] };
+      }
+      return { ...acc, [key]: [file.type, value[0]] };
+    }, {});
   return getObj(treeDeep);
 };
 
