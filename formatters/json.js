3f342f768ef59f910220d09d6da50fff7be9e32d
Vyacheslav Maletin <Maletin.V95@gmail.com>
2022-01-14 01:04:30 +0500

    step 8.4

--- formatters/json.js
+++ formatters/json.js
@@ -1,53 +1,18 @@
 import _ from 'lodash';
-import parsers from '../src/parsers.js';
 
 const json = (treeDeep) => {
-  const previous = [null];
-  const closingBracket = [false];
-  const getStyle = (tree) => tree
+  const getObj = (tree) => tree
     .reduce((acc, file) => {
+      const key = !file.name ? Object.keys(file)[0] : file.name;
       const value = [];
-      if (file.children === 'null' || typeof file.children === 'number') {
-        value[0] = file.children === 'null' ? null : _.cloneDeep(file.children);
+      if (Array.isArray(file.children)) {
+        value[0] = getObj(file.children);
       } else {
-        value[0] = JSON.stringify(_.cloneDeep(file.children));
+        value[0] = file.children === 'null' ? null : _.cloneDeep(file.children);
       }
-
-      if (file.type === 'general' && Array.isArray(file.children)) {
-        return `${acc}{"name":"${file.name}","type":"notChanged","value":[${getStyle(file.children)}]}`;
-      } if (file.type === 'deleted') {
-        if (file.beenUpdated) {
-          previous[0] = _.cloneDeep(file);
-          return acc;
-        }
-        return `${acc}{"name":"${file.name}","type":"removed","value":${value[0]}}`;
-      } if (file.type === 'added') {
-        if (previous[0] !== null) {
-          const oldValue = [];
-          if (previous[0].children === 'null' || typeof previous[0].children === 'number') {
-            oldValue[0] = previous[0].children === 'null' ? null : _.cloneDeep(previous[0].children);
-          } else {
-            oldValue[0] = JSON.stringify(_.cloneDeep(previous[0].children));
-          }
-          const newValue = value[0];
-          previous[0] = null;
-          return `${acc}{"name":"${file.name}","type":"updated","oldValue":${oldValue},"newValue":${newValue}}`;
-        }
-        return `${acc}{"name":"${file.name}","type":"added","value":${value[0]}}`;
-      }
-      return `${acc}{"name":"${file.name}","type":"notChanged","value":${value[0]}}`;
-    }, '');
-  return parsers(`[${getStyle(treeDeep)}]`.split('')
-    .map((char) => {
-      if (char === '}') {
-        closingBracket[0] = true;
-      } if (char === '{' && closingBracket[0]) {
-        closingBracket[0] = false;
-        return `,${char}`;
-      }
-      return char;
-    })
-    .join(''), 'json');
+      return [...acc, { name: key, type: file.type, value: value[0] }];
+    }, []);
+  return getObj(treeDeep);
 };
 
 export default json;
